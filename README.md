4e786eae989c9a3b9246e501a0fe5b39b3c4d0b6
Ken Pepple <ken@pepple.info>
2013-07-27 17:04:54 -0700

    initial import of workflow

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-prowl.alfredworkflow
+Alfred 2 Workflow for Prowl
 ====================
 
 Alfred workflow to send Prowl alerts
+
+Requires [http://www.alfredapp.com/powerpack/](Alfred 2 and the Powerpack).
---
+++ keyutils.py
@@ -0,0 +1,37 @@
+import ConfigParser
+import os
+import plistlib
+
+def get_bundle_id():
+    info_path = os.path.abspath("./info.plist")
+    if os.path.exists(info_path):
+        info = plistlib.readPlist(info_path)
+        try:
+            this_bundle_id = info["bundleid"]
+        except KeyError:
+            raise Exception("Bundle ID not defined or readable from info.plist.")
+    else:
+        raise Exception("info.plist missing.")
+    return this_bundle_id
+
+def get_config_path(config_file=None):
+    bundle_id = get_bundle_id()
+    nvPath = os.path.expanduser(os.path.join("~/Library/Application Support/Alfred 2/Workflow Data/", bundle_id))
+    if not os.path.exists(nvPath):
+        os.makedirs(nvPath)
+    if config_file:
+        nvPath = os.path.join(nvPath, config_file)
+    return nvPath
+
+def get_config(config_key, section="defaults", config_file="config.ini"):
+    config = ConfigParser.RawConfigParser()
+    config.read(get_config_path(config_file))
+    return config.get(section, config_key)
+
+def save_config(apikey):
+    config = ConfigParser.RawConfigParser()
+    config.add_section('defaults')
+    config.set('defaults', 'apikey', apikey)
+    with open(get_config_path("config.ini"), 'wb') as configfile:
+        config.write(configfile)
+
