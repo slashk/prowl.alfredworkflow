85c463ead3c4ceaed30e2ad28832bd5ef6d800dd
Ken Pepple <ken@pepple.info>
2013-07-27 18:27:19 -0700

    cleanup prowl send

--- prowl_alfred.py
+++ prowl_alfred.py
@@ -35,12 +35,10 @@ def save_api_key(apikey):
     utils.save_config('apikey',apikey)
 
 def send_prowl(description, application="Alfred", event="event", priority=0):
-    prowl_url = "http://api.prowlapp.com/publicapi/add"
     try:
         apikey = get_api_key()
     except:
-        print "No APIKEY. Please configure by holding down the cmd key"\
-              "and pasting in prowl APIKEY."
+        raise.Exception "No APIKEY. Please configure by holding down the cmd key and pasting in prowl APIKEY."
     parameters = {'apikey': apikey, 'event': event, 'application': application,
                   'priority': priority, 'description': description}
     r = requests.post(PROWL_URL + "add", params=parameters)
