64bfe281de022b50b55aea5b671a519d8d16e535
Ken Pepple <ken@pepple.info>
2013-07-27 18:09:33 -0700

    added licenses

--- alfred_utils.py
+++ alfred_utils.py
@@ -1,3 +1,19 @@
+# Copyright 2013 Ken Pepple <ken@pepple.info>
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 import ConfigParser
 import os
 import plistlib
@@ -9,19 +25,19 @@ def get_bundle_id():
         try:
             this_bundle_id = info["bundleid"]
         except KeyError:
-            raise Exception("Bundle ID not defined or readable from info.plist.")
+            raise Exception("Bundle ID not defined")
     else:
-        raise Exception("info.plist missing.")
+        raise Exception("Error reading info.plist!")
     return this_bundle_id
 
 def get_config_path(config_file=None):
     bundle_id = get_bundle_id()
-    nvPath = os.path.expanduser(os.path.join("~/Library/Application Support/Alfred 2/Workflow Data/", bundle_id))
-    if not os.path.exists(nvPath):
-        os.makedirs(nvPath)
+    this_config_path = os.path.expanduser(os.path.join("~/Library/Application Support/Alfred 2/Workflow Data/", bundle_id))
+    if not os.path.exists(this_config_path):
+        os.makedirs(this_config_path)
     if config_file:
-        nvPath = os.path.join(nvPath, config_file)
-    return nvPath
+        this_config_path = os.path.join(this_config_path, config_file)
+    return this_config_path
 
 def get_config(config_key, section="defaults", config_file="config.ini"):
     config = ConfigParser.RawConfigParser()
--- prowl_alfred.py
+++ prowl_alfred.py
@@ -1,3 +1,19 @@
+# Copyright 2013 Ken Pepple <ken@pepple.info>
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 import alfred_utils as utils
 import requests
 
@@ -21,9 +37,9 @@ def save_api_key(apikey):
 def send_prowl(description, application="Alfred", event="event", priority=0):
     prowl_url = "http://api.prowlapp.com/publicapi/add"
     try:
-    	apikey = get_api_key()
+        apikey = get_api_key()
     except:
-    	print "No APIKEY. Please configure by holding down the cmd key"\
+        print "No APIKEY. Please configure by holding down the cmd key"\
               "and pasting in prowl APIKEY."
     parameters = {'apikey': apikey, 'event': event, 'application': application,
                   'priority': priority, 'description': description}
